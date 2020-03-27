84d9ef115837ae966734082f41a4d21e67b9c90c
unknown <2567444258@qq.com>
2020-03-27 20:56:41 +0800

    914.卡牌分组

---
+++ 914_卡牌分组.cpp
@@ -0,0 +1,76 @@
+/*
+给定一副牌，每张牌上都写着一个整数。
+
+此时，你需要选定一个数字 X，使我们可以将整副牌按下述规则分成 1 组或更多组：
+
+每组都有 X 张牌。
+组内所有的牌上都写着相同的整数。
+仅当你可选的 X >= 2 时返回 true。
+
+ 
+
+示例 1：
+
+输入：[1,2,3,4,4,3,2,1]
+输出：true
+解释：可行的分组是 [1,1]，[2,2]，[3,3]，[4,4]
+示例 2：
+
+输入：[1,1,1,2,2,2,3,3]
+输出：false
+解释：没有满足要求的分组。
+示例 3：
+
+输入：[1]
+输出：false
+解释：没有满足要求的分组。
+示例 4：
+
+输入：[1,1]
+输出：true
+解释：可行的分组是 [1,1]
+示例 5：
+
+输入：[1,1,2,2,2,2]
+输出：true
+解释：可行的分组是 [1,1]，[2,2]，[2,2]
+
+提示：
+
+1 <= deck.length <= 10000
+0 <= deck[i] < 10000
+
+*/
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        if (deck.size() <= 1) {
+            return false;
+        }
+        int rec[10001] = {0};
+        for (int i : deck) {
+            ++rec[i];
+        }
+        vector<int> dd;
+        for (int i = 0; i < 10001; ++i) {
+            if (rec[i] != 0) {
+                dd.push_back(rec[i]);
+            }
+        }
+        int min = dd[0];
+        for (int i : dd) {
+            min = gcd(min, i);
+        }
+        if (min == 1 || min == 0) {
+            return false;
+        }
+        
+        return true;
+    }
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+};
