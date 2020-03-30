c3abe0ab2854e4c451fa5ef6f275f66c7909e4a4
unknown <2567444258@qq.com>
2020-03-30 21:10:31 +0800

    面试题62_圆圈中最后剩下的数字

---
+++ 面试题62_圆圈中最后剩下的数字.cpp
@@ -0,0 +1,73 @@
+/*
+0,1,,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字。
+
+例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+
+ 
+
+示例 1：
+
+输入: n = 5, m = 3
+输出: 3
+示例 2：
+
+输入: n = 10, m = 17
+输出: 2
+ 
+
+限制：
+
+1 <= n <= 10^5
+1 <= m <= 10^6
+
+*/
+//递归
+class Solution {
+    int f(int n, int m) {
+        if (n == 1)
+            return 0;
+        int x = f(n - 1, m);
+        return (m + x) % n;
+    }
+public:
+    int lastRemaining(int n, int m) {
+        return f(n, m);
+    }
+};
+//迭代
+/*
+class Solution {
+public:
+    int lastRemaining(int n, int m) {
+        int f = 0;
+        for (int i = 2; i != n + 1; ++i)
+            f = (m + f) % i;
+        return f;
+    }
+};
+
+*/
+
+
+
+/*
+最终剩下一个人时的安全位置肯定为0，反推安全位置在人数为n时的编号
+人数为1： 0
+人数为2： (0+m) % 2
+人数为3： ((0+m) % 2 + m) % 3
+人数为4： (((0+m) % 2 + m) % 3 + m) % 4
+........
+迭代推理到n就可以得出答案
+
+class Solution {
+public:
+    int lastRemaining(int n, int m) {
+        int result = 0;
+        for (int i = 2; i <= n; ++i) {
+            result = (result + m) % i;
+        }
+        return result;
+    }
+};
+
+*/
