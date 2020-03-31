42ca483071aaf354776621db22bca17a407612f4
unknown <2567444258@qq.com>
2020-03-31 20:43:08 +0800

    912.排序数组

---
+++ 912_排序数组.cpp
@@ -0,0 +1,59 @@
+/*
+给你一个整数数组 nums，请你将该数组升序排列。
+
+ 
+
+示例 1：
+
+输入：nums = [5,2,3,1]
+输出：[1,2,3,5]
+示例 2：
+
+输入：nums = [5,1,1,2,0,0]
+输出：[0,0,1,1,2,5]
+
+*/
+// int n = nums.size();
+// int tmp;
+// for (int i = 0; i < n; ++i) {
+//     for (int j = 0; j < n - i - 1; ++j) {
+//         if (nums[j + 1] < nums[j]) {
+//             tmp = nums[j + 1];
+//             nums[j + 1] = nums[j];
+//             nums[j] = tmp;
+//         }
+//     }
+// }
+// return nums;
+class Solution {
+    int partition(vector<int>& nums, int l, int r) {
+        int pivot = nums[r];
+        int i = l - 1;
+        for (int j = l; j <= r - 1; ++j) {
+            if (nums[j] <= pivot) {
+                i = i + 1;
+                swap(nums[i], nums[j]);
+            }
+        }
+        swap(nums[i + 1], nums[r]);
+        return i + 1;
+    }
+    int randomized_partition(vector<int>& nums, int l, int r) {
+        int i = rand() % (r - l + 1) + l; // 随机选一个作为我们的主元
+        swap(nums[r], nums[i]);
+        return partition(nums, l, r);
+    }
+    void randomized_quicksort(vector<int>& nums, int l, int r) {
+        if (l < r){
+            int pos = randomized_partition(nums, l, r);
+            randomized_quicksort(nums, l, pos - 1);
+            randomized_quicksort(nums, pos + 1, r);
+        }
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        srand((unsigned)time(NULL));
+        randomized_quicksort(nums, 0, (int)nums.size() - 1);
+        return nums;
+    }
+};
