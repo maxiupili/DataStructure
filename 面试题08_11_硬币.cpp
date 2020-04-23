37489563474be572bf6c080489e7c50e31fa2dd7
unknown <2567444258@qq.com>
2020-04-23 20:40:34 +0800

    面试题 08.11. 硬币

---
+++ 面试题08_11_硬币.cpp
@@ -0,0 +1,39 @@
+/*
+硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
+
+示例1:
+
+ 输入: n = 5
+ 输出：2
+ 解释: 有两种方式可以凑成总金额:
+5=5
+5=1+1+1+1+1
+示例2:
+
+ 输入: n = 10
+ 输出：4
+ 解释: 有四种方式可以凑成总金额:
+10=10
+10=5+5
+10=5+1+1+1+1+1
+10=1+1+1+1+1+1+1+1+1+1
+
+*/
+class Solution {
+private:
+    static constexpr int mod = 1000000007;
+    static constexpr int coins[4] = {25, 10, 5, 1};
+
+public:
+    int waysToChange(int n) {
+        vector<int> f(n + 1);
+        f[0] = 1;
+        for (int c = 0; c < 4; ++c) {
+            int coin = coins[c];
+            for (int i = coin; i <= n; ++i) {
+                f[i] = (f[i] + f[i - coin]) % mod;
+            }
+        }
+        return f[n];
+    }
+};
